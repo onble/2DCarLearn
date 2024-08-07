ee528e0720495f5b255b1671dc0ca0e522bcaa72
“onble” <62949816+onble@users.noreply.github.com>
2024-08-08 07:42:43 +0800

    完成08课程

--- src/MainScene/Player.ts
+++ src/MainScene/Player.ts
@@ -6,6 +6,9 @@ export class Player extends Laya.Script {
 
     /** 运动刚体组件 */
     private _rig: Laya.RigidBody;
+    /** 限制小汽车的横向移动距离 */
+    private _playerMinx = 200;
+    private _playerMaxX = 880;
 
     //组件被激活后执行，此时所有节点和组件均已创建完毕，此方法只执行一次
     onAwake(): void {
@@ -19,16 +22,17 @@ export class Player extends Laya.Script {
         let force = 0;
         if (mouseX < Laya.stage.width / 2) {
             // left
-            console.log("left");
             force = -1;
         } else {
-            console.log("right");
             force = 1;
         }
-        this._rig.linearVelocity = { x: force * 6, y: 0 };
+        this._rig.linearVelocity = { x: force * 8, y: 0 };
+        // 旋转车头
+        Laya.Tween.to(this.owner, { rotation: force * 25 }, 300);
     }
     MouseUp() {
         this._rig.linearVelocity = { x: 0, y: 0 };
+        Laya.Tween.to(this.owner, { rotation: 0 }, 300);
     }
 
     //组件被启用后执行，例如节点被添加到舞台后
@@ -44,7 +48,13 @@ export class Player extends Laya.Script {
     //onDestroy(): void {}
 
     //每帧更新时执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
-    //onUpdate(): void {}
+    onUpdate(): void {
+        if (this.owner.x > this._playerMaxX) {
+            this.owner.x = this._playerMaxX;
+        } else if (this.owner.x < this._playerMinx) {
+            this.owner.x = this._playerMinx;
+        }
+    }
 
     //每帧更新时执行，在update之后执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
     //onLateUpdate(): void {}
